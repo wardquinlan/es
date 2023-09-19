97e32e2f6c6783247a7ec515c97d82e7e5669858
Ward Quinlan <wardquinlan@outlook.com>
2023-09-19 19:09:57 -0400

    output.stable

--- update1.sh
+++ update1.sh
@@ -2,7 +2,7 @@
 
 JAVA_HOME=/home/ward/java-se-8u41-ri
 export JAVA_HOME
-ES_HOME=/home/ward/workspace/econ/output
+ES_HOME=/home/ward/workspace/econ/output.stable
 export ES_HOME
 
 $ES_HOME/es.sh /home/ward/es/update1.es
