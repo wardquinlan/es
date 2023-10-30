060618e0bf04b2603dcd0b867c1d2cd2ff2cd0d9
Ward Quinlan <wardquinlan@outlook.com>
2023-10-30 15:56:14 -0400

    refresh all

---
+++ refreshAll.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+JAVA_HOME=/home/ward/java-se-8u41-ri
+export JAVA_HOME
+ES_HOME=/home/ward/workspace/econ/output
+export ES_HOME
+
+$ES_HOME/es.sh --admin --command ':SetLogLevel(DEBUG);ES:RefreshAll();'
+
